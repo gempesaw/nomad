3f79aacda3e9d7822e3f74f6d8a4cdc149607ab2
Daniel Gempesaw <dgempesaw@pagerduty.com>
2020-03-27 18:45:52 -0400

    Remove everything but the docs

--- README.md
+++
@@ -1,1 +0,0 @@
-i would like to look at the v0.8.4 docs 
--- ui/app/helpers/format-duration.js
+++
@@ -1,8 +0,0 @@
-import Helper from '@ember/component/helper';
-import formatDuration from '../utils/format-duration';
-
-function formatDurationHelper([duration], { units }) {
-  return formatDuration(duration, units);
-}
-
-export default Helper.helper(formatDurationHelper);
